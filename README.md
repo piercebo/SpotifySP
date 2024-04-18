b7b897a0da249365abfaad5d6200008d50e69458
piercebo <98802866+piercebo@users.noreply.github.com>
2024-04-18 04:25:20 -0400

    Expanded data sets

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
  
 Outside Datasets:
 Spotify API slice - https://www.kaggle.com/datasets/maharshipandya/-spotify-tracks-dataset?resource=download
-Lyric dataset - https://www.kaggle.com/datasets/notshrirang/spotify-million-song-dataset
+Lyric dataset 1 - https://www.kaggle.com/datasets/notshrirang/spotify-million-song-dataset
+Lyric dataset 2 - https://www.kaggle.com/datasets/imuhammad/audio-features-and-lyrics-of-spotify-songs
 
 helper.LyricToCSVFilter() combines the spotify api data with the lyric dataset.  It produces a csv in the datasets directory called "LyricsAndAPI.csv".
 If you are using a CSV highlight tool, the CSV may appear to have broken columns and rows due to commas placed within quotes; The pandas dataframe object will compile it correctly.
--- helper.py
+++ helper.py
@@ -4,10 +4,15 @@ def LyricToCSVFilter(APISlice, lyricSet):
     df1 = pd.read_csv(APISlice)
     df1 = df1.drop(["index"], axis=1)
     df2 = pd.read_csv(lyricSet)
-    df2 = df2.drop(["link"], axis=1)
+    df2 = df2.drop(["track_album_id"], axis=1)
+    df2 = df2.drop(["track_album_name"], axis=1)
+    df2 = df2.drop(["playlist_name"], axis=1)
+    df2 = df2.drop(["playlist_id"], axis=1)
     df1 = df1.merge(df2, how="inner", on=["artists", "track_name"])
-    df1.to_csv("./datasets/LyricsAndAPI.csv", index=False)
+    df1.to_csv("./datasets/LyricsAndAPI2.csv", index=False)
+    
+    print(df1.shape)
     
 
 
-LyricToCSVFilter("./datasets/SpotifyAPISlice.csv", "./datasets/LyricSet.csv")
+LyricToCSVFilter("./datasets/SpotifyAPISlice.csv", "./datasets/LyricSet2.csv")
