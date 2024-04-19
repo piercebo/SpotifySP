e5727c1139e0a79b5a574ba781293d8301d18716
piercebo <98802866+piercebo@users.noreply.github.com>
2024-04-18 23:30:26 -0400

    Added dolchFilter, refined dataset

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Lyric dataset 2 - https://www.kaggle.com/datasets/imuhammad/audio-features-and-l
 
 helper.LyricToCSVFilter() combines the spotify api data with the lyric dataset.  It produces a csv in the datasets directory called "LyricsAndAPI.csv".
 If you are using a CSV highlight tool, the CSV may appear to have broken columns and rows due to commas placed within quotes; The pandas dataframe object will compile it correctly.
+
+helper.dolchFilter() removes 79 of the most common English words.  In order to make a word embedding that embodies the true meaning of a song you must identify words that carry the most symbolism.  For our purposes and for a faster and more accurate algotithm, the dolchFilter() function removes the most common meaningless words from our text source.
--- helper.py
+++ helper.py
@@ -1,18 +1,35 @@
 import pandas as pd
+import re
 
-def LyricToCSVFilter(APISlice, lyricSet):
-    df1 = pd.read_csv(APISlice)
-    df1 = df1.drop(["index"], axis=1)
+def LyricToCSVFilter(lyricSet):
     df2 = pd.read_csv(lyricSet)
     df2 = df2.drop(["track_album_id"], axis=1)
     df2 = df2.drop(["track_album_name"], axis=1)
     df2 = df2.drop(["playlist_name"], axis=1)
+    df2 = df2.drop(["track_popularity"], axis=1)
+    df2 = df2.drop(["track_album_release_date"], axis=1)
     df2 = df2.drop(["playlist_id"], axis=1)
-    df1 = df1.merge(df2, how="inner", on=["artists", "track_name"])
-    df1.to_csv("./datasets/LyricsAndAPI2.csv", index=False)
-    
-    print(df1.shape)
+    df2 = df2.drop(["playlist_genre"], axis=1)
+    df2 = df2.drop(["playlist_subgenre"], axis=1)
+    df2 = df2[df2['language'] == 'en']
+    df2.to_csv("./datasets/LyricsAndAPI2.csv", index=False)
     
 
 
-LyricToCSVFilter("./datasets/SpotifyAPISlice.csv", "./datasets/LyricSet2.csv")
+
+def dolchFilter(csvFile):
+    df = pd.read_csv(csvFile)
+    regexList = [r'\bthe\b', r'\bis\b', r'\bbe\b', r'\bto\b', r'\bof\b', r'\band\b', r'\ba\b', r'\bin\b', r'\bthat\b', r'\bhave\b', r'\bit\b', r'\bfor\b', r'\bon\b', r'\bwith\b', r'\bas\b', r'\bdo\b', r'\bat\b', r'\bthis\b',
+                 r'\bbut\b', r'\bby\b', r'\bfrom\b', r'\bthey\b', r'\bwe\b', r'\bsay\b', r'\bor\b', r'\ban\b', r'\bwill\b', r'\bmy\b', r'\b\b', r'\ball\b', r'\bwould\b', r'\bthere\b', r'\btheir\b', r'\bwhat\b', r'\bso\b', r'\bup\b', r'\bout\b',
+                 r'\bif\b', r'\babout\b', r'\bwho\b', r'\bget\b', r'\bwhich\b', r'\bgo\b', r'\bme\b', r'\bwhen\b', r'\bmake\b', r'\bcan\b', r'\blike\b', r'\btime\b', r'\bno\b', r'\bjust\b', r'\bknow\b', r'\btake\b', r'\binto\b', r'\bsome\b', r'\bcould\b',
+                 r'\bthem\b', r'\bsee\b', r'\bother\b', r'\bthan\b', r'\bthen\b', r'\bnow\b', r'\blook\b', r'\bonly\b', r'\bcome\b', r'\bits\b', r'\bover\b', r'\bthink\b', r'\balso\b', r'\bback\b', r'\bafter\b', r'\buse\b', r'\bhow\b', r'\bour\b', r'\beven\b', r'\bany\b',
+                 r'\bthese\b', r'\bmost\b', r'\bus\b'] 
+    whitespaces = r'\s+'
+    print(len(regexList))
+    for i in range(len(df['text'])):
+        for regex in regexList:
+            df.loc[i, "text"] = re.sub(regex, "", df['text'][i], flags=re.IGNORECASE)
+        df.loc[i, "text"] = re.sub(whitespaces, " ", df['text'][i])
+    df.to_csv("./datasets/FilteredLyrics1.csv", index=False)
+
+dolchFilter("./datasets/LyricsAndAPI2.csv")
